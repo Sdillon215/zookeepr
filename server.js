937eb1bbd0204bbdb30985d4c687ccda87c75688
Sean <sdillon215@gmail.com>
2021-01-29 13:32:48 -0700

    lesson code refactor

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -107,6 +107,10 @@ app.get('/animals', (req, res) => {
 
 app.get('/zookeepers', (req, res) => {
   res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
