be1e08cf12034b34b0f44af786fa79c57e0be00e
Sean <sdillon215@gmail.com>
2021-08-17 17:53:48 -0600

    port changed

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animals.json');
 
@@ -49,5 +50,5 @@ app.get('/api/animals', (req, res) => {
 });
 
 app.listen(3001, () => {
-    console.log(`API server now on port 3001`);
+    console.log(`API server now on port ${port}!`);
 });
