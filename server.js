368fb49f4072d532826ef33285f4153766ec482a
Sean <sdillon215@gmail.com>
2021-08-18 17:09:30 -0600

    added ability to add animals

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+// parse incoming json data
+app.use(express.json());
 const { animals } = require('./data/animals.json');
 
 function filterByQuery(query, animalsArray) {
@@ -46,6 +52,17 @@ function findById(id, animalsArray) {
     return result;
 };
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({animals: animalsArray}, null, 2)
+    );
+
+    return animal;
+};
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -61,6 +78,16 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
         res.sendStatus(404);
     }
+});
+
+app.post ('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+
+    res.json(req.body);
 });
 
 app.listen(PORT, () => {
