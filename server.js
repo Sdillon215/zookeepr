262817bef030321620155eef8965c4ba3f3a413d
Sean <sdillon215@gmail.com>
2021-08-18 17:18:23 -0600

    validation added

--- server.js
+++ server.js
@@ -59,8 +59,23 @@ function createNewAnimal(body, animalsArray) {
         path.join(__dirname, './data/animals.json'),
         JSON.stringify({animals: animalsArray}, null, 2)
     );
-
     return animal;
+};
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
 };
 
 app.get('/api/animals', (req, res) => {
@@ -84,10 +99,13 @@ app.post ('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
 
-    // add animal to json file and animals array in this function
-    const animal = createNewAnimal(req.body, animals);
-
-    res.json(req.body);
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
 });
 
 app.listen(PORT, () => {
